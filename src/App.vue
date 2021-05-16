912fc2ab31ba75f20edaac7b07a5bb6262bd459b
kenzo44 <kenzo44@users.noreply.github.com>
2021-05-16 00:30:05 -0600

    Housekeeping

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
+<Header />
+<div class="container">
+  
   <router-view/>
 </template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
