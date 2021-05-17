e63d22d3483db243be4f7d43058c8f4d3173febe
kenzo44 <kenzo44@users.noreply.github.com>
2021-05-17 16:19:43 -0600

    Added country selector

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
     <main v-if="!loading">
         <DataTitle :text="title" :dataDate="dataDate"/>
+
         <DataBoxes :stats="stats"/>
+
+        <CountrySelect @get-country="getCountryData" :countries="countries"/>
     </main>
     <main class="flex flex-col align-center justify-center text-center" v-else>
         <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -14,12 +17,14 @@
 <script>
  import DataTitle from '@/components/DataTitle'
  import DataBoxes from '@/components/DataBoxes'
+ import CountrySelect from '@/components/CountrySelect'
  
  export default {
      name: "Home",
      components: {
          DataTitle,
-         DataBoxes
+         DataBoxes,
+         CountrySelect
      },
      data() {
          return{
@@ -36,7 +41,11 @@
              const res = await fetch('https://api.covid19api.com/summary')
              const data = await res.json()
              return data
-         }  
+         },
+         getCountryData(country) {
+             this.stats = country
+             this.title = country.Country
+         }
      },
      async created(){
          const data = await this.fetchCovidData()
