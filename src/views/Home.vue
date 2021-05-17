91e0dde0ef3cd1128eb90f38c1a0b283bfac9bf5
kenzo44 <kenzo44@users.noreply.github.com>
2021-05-17 16:26:29 -0600

    Added clear button UI needs improvement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,8 @@
         <DataBoxes :stats="stats"/>
 
         <CountrySelect @get-country="getCountryData" :countries="countries"/>
+
+        <button @click="clearCountryData" v-if="stats.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">Clear Country</button>
     </main>
     <main class="flex flex-col align-center justify-center text-center" v-else>
         <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -45,7 +47,14 @@
          getCountryData(country) {
              this.stats = country
              this.title = country.Country
-         }
+         },
+         async clearCountryData() {
+             this.loading = true
+             const data = await this.fetchCovidData()
+             this.title = 'Global'
+             this.stats = data.Global
+             this.loading = false
+         },
      },
      async created(){
          const data = await this.fetchCovidData()
