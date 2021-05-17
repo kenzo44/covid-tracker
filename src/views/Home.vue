17b5ece239f951d93d8ecf1f31134a7669022cc9
kenzo44 <kenzo44@users.noreply.github.com>
2021-05-17 16:01:52 -0600

    Added total cases and deaths with commas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <main v-if="!loading">
         <DataTitle :text="title" :dataDate="dataDate"/>
+        <DataBoxes :stats="stats"/>
     </main>
     <main class="flex flex-col align-center justify-center text-center" v-else>
         <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -12,11 +13,13 @@
 
 <script>
  import DataTitle from '@/components/DataTitle'
+ import DataBoxes from '@/components/DataBoxes'
  
  export default {
      name: "Home",
      components: {
-         DataTitle
+         DataTitle,
+         DataBoxes
      },
      data() {
          return{
