08fa2be37ddaa9ae09bb418b2490bcb11546fd51
kenzo44 <kenzo44@users.noreply.github.com>
2021-05-17 15:26:11 -0600

    Declared variables & implemented fancy loading gif

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="home">Covid Tracker</div>
+    <main v-if="!loading">Cool!</main>
+    <main class="flex flex-col align-center justify-center text-center" v-else>
+        <div class="text-gray-500 text-3xl mt-10 mb-6">
+            Fetching Data
+        </div>
+        <img :src="loadingImage" class="w-24 m-auto" alt="Loading data"/>
+    </main>
 </template>
 
 <script>
 export default {
   name: "Home",
      components: {},
+     data() {
+         return{
+             loading: true,
+             title: 'Global',
+             dataDate: '',
+             stats: {},
+             countries: [],
+             loadingImage: require('../assets/loading.gif')
+         }  
+     },
      methods: {
          async fetchCovidData() {
              const res = await fetch('https://api.covid19api.com/summary')
@@ -15,7 +31,10 @@ export default {
      },
      async created(){
          const data = await this.fetchCovidData()
-         console.log(data)
+         this.dataDate = data.Date
+         this.stats = data.Global
+         this.countries = data.Countries
+         this.loading = false
      },
  };
 </script>
